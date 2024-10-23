cab8e6c70c871bd17ec626e2af7267f219ba6c0f
rasaTizdast <rasaknifehand@gmail.com>
2024-10-23 18:21:56 +0330

    Created a component for the HeaderSubMenu to be client component

---
+++ src/app/(main)/_components/ui/navbar/HeaderSubMenu.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import styles from "./NavBar.module.css";
+
+interface SubMenuItem {
+  id: number;
+  title: string;
+  link: string;
+}
+
+interface MenuItem {
+  id: number;
+  title: string;
+  link: string;
+  subMenu?: SubMenuItem[];
+}
+
+interface HeaderSubMenuProps {
+  data: MenuItem;
+}
+
+const HeaderSubMenu = ({ data }: HeaderSubMenuProps) => {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <li
+      className={styles.nav_item}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <Link href={data.link}>{data.title}</Link>
+      <ul className={hovered ? styles.submenu : styles.hidden}>
+        {data.subMenu?.map((item) => (
+          <li key={item.id}>
+            <Link href={item.link}>{item.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </li>
+  );
+};
+
+export default HeaderSubMenu;
