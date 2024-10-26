a448cc01061b333e09c882a3bec023cfe8068669
rasaTizdast <rasaknifehand@gmail.com>
2024-10-26 11:56:01 +0330

    Created teh HomePage of the website

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+
+import Slider from "../components/Slider";
+import ProductsShowCase from "../components/LandingPage/ProductsShowCase";
+import ProjectsSection from "../components/LandingPage/ProjectsSection";
+import SupportSection from "../components/LandingPage/SupportSection";
+
+export const metadata: Metadata = {
+  title: "پیشرو در تکنولوژی | فرابک",
+  description: "پیشرو در تکنولوژی و نوآوری های فرابک با محصولات و خدمات متنوع.",
+};
+
+const HomePage = () => {
+  return (
+    <div>
+      <Slider />
+      <ProductsShowCase />
+      <ProjectsSection />
+      <SupportSection />
+    </div>
+  );
+};
+
+export default HomePage;
