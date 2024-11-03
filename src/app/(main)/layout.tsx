084774c7246d8c9f181b5722dbb5c363f90d7dd4
rasaTizdast <rasaknifehand@gmail.com>
2024-11-03 16:33:01 +0330

    Removed skeleton loading provider from the root layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,3 @@
-import { SkeletonTheme } from "react-loading-skeleton";
 import Header from "./_components/ui/header/Header";
 import Footer from "./_components/ui/footer/Footer";
 
@@ -8,11 +7,11 @@ const layout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <SkeletonTheme baseColor="#dadada" highlightColor="#f0f0f0">
+    <>
       <Header />
       {children}
       <Footer />
-    </SkeletonTheme>
+    </>
   );
 };
 
