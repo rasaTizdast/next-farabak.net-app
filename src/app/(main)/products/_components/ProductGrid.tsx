eacd4c1fbfad5f9eaf8e2431c502563f71b17066
rasaTizdast <rasaknifehand@gmail.com>
2024-11-04 10:40:31 +0330

    Updated productGrid

--- src/app/(main)/products/_components/ProductGrid.tsx
+++ src/app/(main)/products/_components/ProductGrid.tsx
@@ -1,66 +1,37 @@
 // src/app/(main)/products/_components/ProductGrid.tsx
-import Link from "next/link";
-import Image from "next/image";
 
+import { Suspense } from "react";
+import { GridContentServer } from "./GridContentServer"; // Server component for products
+import SkeletonLoader from "@/app/_components/ui/SkeletonLoader";
 import styles from "./ProductGrid.module.css";
-import Pagination from "@/app/_components/Pagination";
-
-interface Product {
-  ProductId: number;
-  Type: string;
-  img1: string;
-}
 
 interface ProductGridProps {
   title: string;
-  products: Product[];
+  apiUrl: string;
   currentPage: number;
-  totalPages: number;
   categorySlug?: string;
-  subcategorySlug?: string; // Add subcategorySlug here
+  subcategorySlug?: string;
 }
 
 const ProductGrid: React.FC<ProductGridProps> = ({
   title,
-  products,
+  apiUrl,
   currentPage,
-  totalPages,
   categorySlug,
-  subcategorySlug, // Destructure subcategorySlug here
+  subcategorySlug,
 }) => {
   return (
     <div className={styles.gridContainer}>
       <h1 className={styles.gridTitle}>{title}</h1>
-      <div className={styles.productGrid}>
-        {products.map((product) => (
-          <Link
-            key={product.ProductId}
-            href={`/products/${categorySlug ? `${categorySlug}/` : ""}${
-              subcategorySlug ? `${subcategorySlug}/` : ""
-            }${product.ProductId}`}
-            className={styles.productCard}
-          >
-            <Image
-              width={280}
-              height={280}
-              quality={100}
-              src={`/productImages/${product.img1}`}
-              alt={product.Type}
-              loading="eager"
-            />
-            <h2>{product.Type}</h2>
-          </Link>
-        ))}
-      </div>
 
-      {/* Pagination Controls */}
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        basePath="/products"
-        categorySlug={categorySlug}
-        subcategorySlug={subcategorySlug} // Pass subcategorySlug to Pagination
-      />
+      <Suspense fallback={<SkeletonLoader amount={15} />}>
+        <GridContentServer
+          apiUrl={apiUrl}
+          currentPage={currentPage}
+          categorySlug={categorySlug}
+          subcategorySlug={subcategorySlug}
+        />
+      </Suspense>
     </div>
   );
 };
