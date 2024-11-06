1acb7a689024f21d5d094c8fe7be0adda2726101
rasaTizdast <rasaknifehand@gmail.com>
2024-11-06 14:47:06 +0330

    Removed hardcoded sliders data for the landing page.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -11,9 +11,42 @@ export const metadata: Metadata = {
 };
 
 const HomePage = () => {
+  const sliderLinks = [
+    {
+      id: 1,
+      img: "/slider-imgs/1.webp",
+      link: "/products/category/home-edition",
+      alt: "",
+    },
+    {
+      id: 2,
+      img: "/slider-imgs/2.webp",
+      link: "/products/category/home-edition?q=dome",
+      alt: "",
+    },
+    {
+      id: 3,
+      img: "/slider-imgs/3.webp",
+      link: "/products/category/home-edition?q=ip",
+      alt: "",
+    },
+    {
+      id: 4,
+      img: "/slider-imgs/4.webp",
+      link: "/products/category/home-edition?q=battery",
+      alt: "",
+    },
+    {
+      id: 5,
+      img: "/slider-imgs/5.webp",
+      link: "/products/category/home-edition",
+      alt: "",
+    },
+  ];
+
   return (
     <div>
-      <Slider />
+      <Slider slides={sliderLinks} />
       <ProductsShowCase />
       <ProjectsSection />
       <SupportSection />
