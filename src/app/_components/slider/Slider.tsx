064e5b5b0590193a1a6cde19f04f6e4e1a3f2437
rasaTizdast <rasaknifehand@gmail.com>
2024-11-06 14:46:23 +0330

    updated slider component

--- src/app/_components/slider/Slider.tsx
+++ src/app/_components/slider/Slider.tsx
@@ -17,47 +17,23 @@ import styles from "./Slider.module.css";
 import Link from "next/link"; // Use Next.js Link for routing
 import Image from "next/image"; // Import Image from Next.js
 
-const sliderLinks = [
-  {
-    id: 1,
-    img: "/slider-imgs/1.webp",
-    link: "/products/category/home-edition",
-    alt: "",
-  },
-  {
-    id: 2,
-    img: "/slider-imgs/2.webp",
-    link: "/products/category/home-edition?q=dome",
-    alt: "",
-  },
-  {
-    id: 3,
-    img: "/slider-imgs/3.webp",
-    link: "/products/category/home-edition?q=ip",
-    alt: "",
-  },
-  {
-    id: 4,
-    img: "/slider-imgs/4.webp",
-    link: "/products/category/home-edition?q=battery",
-    alt: "",
-  },
-  {
-    id: 5,
-    img: "/slider-imgs/5.webp",
-    link: "/products/category/home-edition",
-    alt: "",
-  },
-];
+type Slides = {
+  slides: {
+    id: number;
+    img: string;
+    link: string;
+    alt: string;
+  }[];
+};
 
-const Slider = () => {
+const Slider = ({ slides }: Slides) => {
   return (
     <div className={styles.slider_container}>
       <Swiper
         className={styles.swiper_container}
         modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, Virtual]}
         virtual={{ cache: true, enabled: true }}
-        // autoplay={{ delay: 4500 }}
+        autoplay={{ delay: 4500 }}
         a11y={{
           prevSlideMessage: "Previous slide",
           nextSlideMessage: "Next slide",
@@ -68,7 +44,7 @@ const Slider = () => {
         slidesPerView={1}
         pagination={{ clickable: true }}
       >
-        {sliderLinks.map((slider) => (
+        {slides.map((slider) => (
           <SwiperSlide key={slider.id} className={styles.swiper_slide}>
             <Link href={slider.link}>
               <Image
