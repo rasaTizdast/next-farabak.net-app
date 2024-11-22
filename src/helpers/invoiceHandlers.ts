15ca20da6ddbba4543ac302554cc8e5a202a9fb1
rasaTizdast <rasaknifehand@gmail.com>
2024-11-22 14:50:04 +0330

    Added new invoiceHandlers

--- src/helpers/invoiceHandlers.ts
+++ src/helpers/invoiceHandlers.ts
@@ -3,14 +3,37 @@ import axios from "axios";
 export const addNewInvoice = async (invoice, user) => {
   const productNames = invoice.products.map((p) => p.name);
   const productAmounts = invoice.products.map((p) => p.amount);
+  try {
+    const res = axios.post("/api/invoice", {
+      Fullname: `${user.firstName} ${user.lastName}`,
+      Phonenumber: user.phoneNumber,
+      TotalAmount: invoice.totalItems,
+      ProductName: productNames.toString(),
+      Quantity: productAmounts.toString(),
+      UserId: user.userId,
+    });
+    return res;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
 
-  const res = axios.post("/api/invoice", {
-    Fullname: `${user.firstName} ${user.lastName}`,
-    Phonenumber: user.phoneNumber,
-    TotalAmount: invoice.totalItems,
-    ProductName: productNames.toString(),
-    Quantity: productAmounts.toString(),
-    UserId: user.userId,
-  });
-  return res;
+export const getuserInvoices = async () => {
+  try {
+    const res = axios.get("/api/invoice");
+    return (await res).data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const checkUserInvoice = async (guid: string) => {
+  try {
+    const res = axios.patch("/api/invoice", {
+      FactorGuid: guid,
+    });
+    return (await res).data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
