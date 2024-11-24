e8a36f632fdb81207a5a32a678f9189ede6eb94a
rasaTizdast <rasaknifehand@gmail.com>
2024-11-24 21:28:03 +0330

    Removed previous slider, and developed new image slider

---
+++ src/app/_components/imageSlider/ImageSlider.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
+
+type Slider = {
+  id: number;
+  img: string;
+  link: string;
+  alt: string;
+};
+
+type ImageSliderProps = {
+  slides: Slider[];
+  interval?: number; // Optional prop for autoplay interval in milliseconds
+};
+
+const ImageSlider = ({ slides, interval }: ImageSliderProps) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isPaused, setIsPaused] = useState(false);
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  // Autoplay logic
+  useEffect(() => {
+    if (isPaused) return; // Stop autoplay when paused
+
+    const autoplay = setInterval(() => {
+      nextSlide();
+    }, interval || 3000);
+
+    return () => clearInterval(autoplay); // Clear interval on cleanup
+  }, [currentIndex, isPaused, interval]);
+
+  return (
+    <div
+      className="max-w-[1400px] h-auto max-h-[calc(100vh-65px)] w-full m-auto relative group overflow-hidden"
+      onMouseEnter={() => setIsPaused(true)} // Pause autoplay on hover
+      onMouseLeave={() => setIsPaused(false)} // Resume autoplay on mouse leave
+    >
+      <div
+        className="flex transition-transform duration-700 ease-in-out"
+        style={{
+          transform: `translateX(${currentIndex * 100}%)`,
+        }}
+      >
+        {slides.map((slide) => (
+          <Link
+            href={slide.link}
+            key={slide.id}
+            className="w-full flex-shrink-0"
+          >
+            <Image
+              src={slide.img}
+              alt={slide.alt}
+              width={1920}
+              height={900}
+              quality={100}
+            />
+          </Link>
+        ))}
+      </div>
+      {/* Left Arrow */}
+      <div
+        onClick={nextSlide}
+        className="hidden group-hover:block absolute top-[45.5%] -translate-x-0 left-5 text-2xl rounded-full p-2 bg-black/30 text-white cursor-pointer"
+      >
+        <BsChevronCompactLeft size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div
+        onClick={prevSlide}
+        className="hidden group-hover:block absolute top-[45.5%] -translate-x-0 right-5 text-2xl rounded-full p-2 bg-black/30 text-white cursor-pointer"
+      >
+        <BsChevronCompactRight size={30} />
+      </div>
+
+      {/* Slider Pagination */}
+      <div className="flex justify-center py-1 px-2 gap-1 absolute left-[50%] -translate-x-[50%] -translate-y-10 bg-[#f0f0f0] rounded-tl-2xl rounded-tr-2xl">
+        {slides.map((slide, slideIndex) => (
+          <div
+            className="text-2xl cursor-pointer"
+            key={slideIndex}
+            onClick={() => setCurrentIndex(slideIndex)}
+          >
+            <RxDotFilled
+              size={33}
+              className={`transition-all ${
+                slideIndex === currentIndex
+                  ? "text-[#000000]"
+                  : "text-[#0e8bff]"
+              }`}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
