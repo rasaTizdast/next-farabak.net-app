60b211e67bd6a9abd97cd276f8f26e5680f5ef7b
rasaTizdast <rasaknifehand@gmail.com>
2024-11-28 18:16:00 +0330

    Updated UserStatusIcon

--- src/app/(main)/_components/ui/header/UserStatusIcon.tsx
+++ src/app/(main)/_components/ui/header/UserStatusIcon.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+
 import { PiUserCircleDashedFill } from "react-icons/pi"; // Icons remain the same
+
 import UserDropDown from "../userDropDown/UserDropDown";
-import styles from "./Header.module.css"; // CSS module remains unchanged
+
 import { useUser } from "@/context/UserContext"; // Access the context directly
+
+import styles from "./Header.module.css"; // CSS module remains unchanged
 
 const UserStatusIcon = () => {
   const router = useRouter();
