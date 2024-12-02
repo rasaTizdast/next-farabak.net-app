8f8ab35e42d8cb077a3247977dc85e1da5d757a1
rasaTizdast <rasaknifehand@gmail.com>
2024-12-02 19:36:34 +0330

    Updated next.config to have these options: {   reactStrictMode: false, // Disable React Strict Mode   images: {     remotePatterns: [       {         protocol: "https",         hostname: "farabak.storage.c2.liara.space", // Correctly specify the hostname       },     ],   }, };

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false, // Disable React Strict Mode
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "farabak.storage.c2.liara.space", // Correctly specify the hostname
+      },
+    ],
+  },
+};
 
 export default nextConfig;
