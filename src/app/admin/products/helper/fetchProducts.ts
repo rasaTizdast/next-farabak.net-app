9cb94cbc613906aa1b2fe7754f4f4fdb871f0c75
rasaTizdast <rasaknifehand@gmail.com>
2024-12-06 21:43:04 +0330

    Updated fetchProducts for admin panel

--- src/app/admin/products/helper/fetchProducts.ts
+++ src/app/admin/products/helper/fetchProducts.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import toast from "react-hot-toast";
+import { hasFilters } from "./hasFilters";
 
 type Product = {
   ProductId: number;
@@ -16,13 +17,14 @@ type Props = {
   page: number;
   setIsLoading: (isLoading: boolean) => void;
   setProducts: (products: Product[]) => void;
-  setFilteredProducts: (products: Product[]) => void;
-  setPagination: (
-    updater: (prev: { totalPages: number; currentPage: number }) => {
-      totalPages: number;
-      currentPage: number;
-    }
-  ) => void;
+  setPagination: (updater: { totalPages: number; currentPage: number }) => void;
+  available?: "all" | "true" | "false";
+  searchQuery?: string;
+  filters?: {
+    category: string;
+    subCategory: string;
+    available: boolean | null;
+  };
 };
 
 // Fetch products function with loading state
@@ -30,23 +32,38 @@ const fetchProducts = async ({
   page,
   setIsLoading,
   setProducts,
-  setFilteredProducts,
   setPagination,
+  searchQuery = "",
+  filters,
 }: Props) => {
   try {
     setIsLoading(true);
-    const response = await axios.get(
-      `/api/products/getAllProducts?page=${page}&limit=30`
-    );
+
+    // Base URL for the API
+    let url = `/api/admin/products?page=${page}&limit=30`;
+
+    // Add query parameter if it exists
+    if (searchQuery.trim().length > 0) {
+      url += `&q=${encodeURIComponent(searchQuery)}`;
+    }
+
+    // Add filters if they have values
+    if (hasFilters(filters)) {
+      const { category, subCategory, available } = filters!;
+      if (category) url += `&category=${category}`;
+      if (subCategory) url += `&subcategory=${subCategory}`;
+      if (available !== null) url += `&available=${available}`;
+    }
+
+    // Fetch data
+    const response = await axios.get(url);
     const { data, pagination } = response.data;
-    const { totalPages } = pagination;
 
     setProducts(data); // Ensure `data` matches the `Product[]` type
-    setFilteredProducts(data);
-    setPagination((prev) => ({
-      ...prev,
-      totalPages,
-    }));
+    setPagination({
+      totalPages: pagination.totalPages,
+      currentPage: pagination.currentPage,
+    });
   } catch (error) {
     console.error("Error fetching products:", error);
     toast.error("خطا در بارگذاری محصولات.");
