8544030b87de99b95548a0662d76528a8c9a0ce7
rasaTizdast <rasaknifehand@gmail.com>
2024-12-08 21:39:23 +0330

    updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        "fade-in": "fade-in 0.3s ease-out",
+      },
+      keyframes: {
+        "fade-in": {
+          from: {
+            opacity: "0",
+            transform: "scale(0.95)",
+          },
+          to: {
+            opacity: "1",
+            transform: "scale(1)",
+          },
+        },
+      },
       screens: {
         "2xl": "1400px",
       },
