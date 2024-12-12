0adcfa1a935fb30328f0a6762bf0349b58f007fc
rasaTizdast <rasaknifehand@gmail.com>
2024-12-12 20:17:53 +0330

    Updated userStatusIcon

--- src/app/(main)/_components/ui/header/UserStatusIcon.tsx
+++ src/app/(main)/_components/ui/header/UserStatusIcon.tsx
@@ -1,22 +1,25 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-
 import { PiUserCircleDashedFill } from "react-icons/pi"; // Icons remain the same
-
 import UserDropDown from "../userDropDown/UserDropDown";
-
 import { useUser } from "@/context/UserContext"; // Access the context directly
-
+import { useEffect, useState } from "react"; // Import useEffect and useState for state management
 import styles from "./Header.module.css"; // CSS module remains unchanged
 
 const UserStatusIcon = () => {
   const router = useRouter();
   const { isLoggedIn } = useUser(); // Use the user context
+  const [userLoggedIn, setUserLoggedIn] = useState(isLoggedIn);
+
+  // Re-render when the isLoggedIn value changes in context
+  useEffect(() => {
+    setUserLoggedIn(isLoggedIn);
+  }, [isLoggedIn]);
 
   return (
     <div className={styles.icons}>
-      {isLoggedIn ? (
+      {userLoggedIn ? (
         <UserDropDown />
       ) : (
         <div className={styles.userIcon}>
