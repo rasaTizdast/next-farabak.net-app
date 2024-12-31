d5216b8bcf197063f438757816087ede977bcef4
rasaTizdast <rasaknifehand@gmail.com>
2024-12-31 12:30:40 +0330

    Updated Prisma to be a singleton pattern

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+declare global {
+  // Prevent multiple PrismaClient instances in development
+  var prisma: PrismaClient | undefined;
+}
 
-export { prisma };
+// Use a global variable to maintain a singleton in development
+export const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
