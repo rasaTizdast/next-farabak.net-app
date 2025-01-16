754428f57d56ed7721d02a2321964ca7f0d13bfb
rasaTizdast <rasaknifehand@gmail.com>
2025-01-16 23:04:04 +0330

    Added placeholder for umami tracker

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "./_components/ui/header/Header";
 import Footer from "./_components/ui/footer/Footer";
 import BackToTop from "../_components/ui/BackToTop";
+import Script from "next/script";
 
 const Layout = ({
   children,
@@ -9,6 +10,11 @@ const Layout = ({
 }>) => {
   return (
     <>
+      {/* <Script
+        defer
+        src="https://cloud.umami.is/script.js"
+        data-website-id="b406fd7a-09ee-41dd-8d82-97c7184b61bf"
+      /> */}
       <Header />
       {children}
       <Footer />
