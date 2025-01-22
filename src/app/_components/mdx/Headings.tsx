b58e523a26d961ad93791d3035dde52b0aad3e17
rasaTizdast <rasaknifehand@gmail.com>
2025-01-22 22:03:30 +0330

    Updated Mdx Components

--- src/app/_components/mdx/Headings.tsx
+++ src/app/_components/mdx/Headings.tsx
@@ -11,31 +11,31 @@ export const H1: React.FC<HeadingProps> = ({ children }) => (
 );
 
 export const H2: React.FC<HeadingProps> = ({ children }) => (
-  <h2 className="text-lg mobile:text-xl sm:text-3xl font-semibold text-gray-800 mt-3 mb-6 leading-normal">
+  <h2 className="text-lg mobile:text-xl sm:text-3xl font-semibold text-gray-800 mt-3 mb-4 leading-normal">
     {children}
   </h2>
 );
 
 export const H3: React.FC<HeadingProps> = ({ children }) => (
-  <h3 className="text-lg mobile:text-xl font-semibold text-gray-800 mt-3 mb-6 leading-normal">
+  <h3 className="text-lg mobile:text-xl font-semibold text-gray-800 my-3 leading-normal">
     {children}
   </h3>
 );
 
 export const H4: React.FC<HeadingProps> = ({ children }) => (
-  <h4 className="text-sm mobile:text-base sm:text-lg font-semibold text-gray-800 mt-3 mb-6 leading-normal">
+  <h4 className="text-sm mobile:text-base sm:text-lg font-semibold text-gray-800 my-3 leading-normal">
     {children}
   </h4>
 );
 
 export const H5: React.FC<HeadingProps> = ({ children }) => (
-  <h5 className="text-xs mobile:text-sm sm:text-base font-semibold text-gray-800 mt-3 mb-6 leading-normal">
+  <h5 className="text-xs mobile:text-sm sm:text-base font-semibold text-gray-800 my-3 leading-normal">
     {children}
   </h5>
 );
 
 export const H6: React.FC<HeadingProps> = ({ children }) => (
-  <h6 className="text-xs sm:text-sm font-semibold text-gray-800 mt-3 mb-6 leading-normal">
+  <h6 className="text-xs sm:text-sm font-semibold text-gray-800 my-3 leading-normal">
     {children}
   </h6>
 );
--- src/app/_components/mdx/Link.tsx
+++ src/app/_components/mdx/Link.tsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 interface LinkProps {
   href: string;
   target?: "blank" | null;
+  title?: "";
   children: React.ReactNode;
 }
 
-export const CustomLink: React.FC<LinkProps> = ({ href, children }) => (
+export const CustomLink: React.FC<LinkProps> = ({ href, children, title }) => (
   <Link
     target="_blank"
     href={href}
+    title={title}
     className="relative inline-block my-3 px-2 rounded-lg text-blue-950 hover:text-[#000000] bg-blue-200 hover:bg-blue-300 transition-all"
   >
     {children}
