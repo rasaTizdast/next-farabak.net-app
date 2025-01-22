1c9333af9b6c68d65fbaddfd011ce49c1d1fc8a7
rasaTizdast <rasaknifehand@gmail.com>
2025-01-22 22:07:52 +0330

    Updated Robots.Ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*", // Applies to all user agents
         allow: "/", // Allow access to all pages
+        disallow: "/admin/",
       },
     ],
     sitemap: "https://farabak.net/sitemap.xml", // URL to your sitemap
