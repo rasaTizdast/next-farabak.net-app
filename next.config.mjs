888d202acea954c6d53da909f23b52c0d0cbd0d2
rasaTizdast <rasaknifehand@gmail.com>
2025-01-27 12:52:17 +0330

    Updated liara and next.config file for liara deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   // reactStrictMode: false, // Disable React Strict Mode
   images: {
     remotePatterns: [
