be695f92dd9f8ed93027fcd70480c2925500f2e2
rasaTizdast <rasaknifehand@gmail.com>
2025-02-05 14:41:36 +0330

    Made product showcase dynamic

--- src/app/_components/LandingPage/ProductsShowCase.tsx
+++ src/app/_components/LandingPage/ProductsShowCase.tsx
@@ -1,10 +1,35 @@
-import Link from "next/link"; // Use Next.js Link for routing
-import Image from "next/image"; // Import Image from Next.js
+import Link from "next/link";
+import Image from "next/image";
 import styles from "./ProductsShowCase.module.css";
-import cardData from "../../../constants/cardData.json"; // Ensure cardData is imported correctly
+import { prisma } from "@/lib/prisma";
 
-const ProductsShowCase = () => {
-  const getRowClass = (index:number) => {
+interface ShowcaseProduct {
+  id: number;
+  title: string;
+  description: string;
+  order: number;
+  image: string;
+  link: string;
+}
+
+async function getProducts() {
+  try {
+    const products = await prisma.showcase_products.findMany({
+      orderBy: {
+        order: "asc",
+      },
+    });
+    return products;
+  } catch (error) {
+    console.error("Error fetching showcase products:", error);
+    throw new Error("Failed to fetch showcase products");
+  }
+}
+
+const ProductsShowCase = async () => {
+  const products = await getProducts();
+
+  const getRowClass = (index: number) => {
     if (index === 0) return styles.oneThird;
     if (index === 1) return styles.twoThirds;
     if (index === 2) return styles.twoThirds;
@@ -16,40 +41,37 @@ const ProductsShowCase = () => {
     <div className={styles.container}>
       <h2>محصولات رئولینک</h2>
       <div className={styles.cards}>
-        {Array.from({ length: 3 }, (_, rowIndex) => (
-          <div className={styles.row} key={rowIndex}>
-            {cardData
-              .slice(rowIndex * 2, rowIndex * 2 + 2)
-              .map((card, cardIndex) => (
-                <Link
-                  key={card.id}
-                  href={card.link}
-                  className={`${styles.card} ${getRowClass(
-                    rowIndex * 2 + cardIndex
-                  )}`}
-                >
-                  <Image
-                    src={card.img}
-                    alt={card.title}
-                    loading="lazy"
-                    height={780}
-                    width={1340}
-                  />
-                  <div className={styles.details}>
-                    <h3>{card.title}</h3>
-                    <p>{card.subTitle}</p>
-                  </div>
-                </Link>
-              ))}
-          </div>
-        ))}
+        {Array.from(
+          { length: Math.ceil(products.length / 2) },
+          (_, rowIndex) => (
+            <div className={styles.row} key={rowIndex}>
+              {products
+                .slice(rowIndex * 2, rowIndex * 2 + 2)
+                .map((product, cardIndex) => (
+                  <Link
+                    key={product.id}
+                    href={product.link}
+                    className={`${styles.card} ${getRowClass(
+                      rowIndex * 2 + cardIndex
+                    )}`}
+                  >
+                    <Image
+                      src={`${process.env.LIARA_BUCKET_URL}/${product.image}`}
+                      alt={product.title}
+                      loading="lazy"
+                      height={780}
+                      width={1340}
+                    />
+                    <div className={styles.details}>
+                      <h3>{product.title}</h3>
+                      <p>{product.description}</p>
+                    </div>
+                  </Link>
+                ))}
+            </div>
+          )
+        )}
       </div>
-      <Link
-        href="/products/home-edition"
-        className={styles.all_home_edition}
-      >
-        مشاهده تمامی محصولات رئولینک
-      </Link>
     </div>
   );
 };
